4ca587aa0da77c3aaff0f887b47fd779f37dd2ea
erica nogueira <ericasnogueira10@gmail.com>
2022-10-03 13:39:26 -0300

    Exercicio Data 3.14

---
+++ src/cap03/exercicicio/Date/application/DateTest.java
@@ -0,0 +1,13 @@
+package cap03.exercicicio.Date.application;
+
+import cap03.exercicicio.Date.entities.Date;
+
+public class DateTest {
+    public static void main(String[] args) {
+        Date date = new Date(15,5,1995);
+
+
+        System.out.println(date.displayDate());
+
+    }
+}
